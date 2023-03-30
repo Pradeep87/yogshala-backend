b954530e321e38a03004455d056233078ac5eaf1
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-03-30 14:11:38 +0530

    server request not sending

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,8 +2,9 @@ const User = require("../models/user")
 
 exports.registerUser = async (req, res) => {
     try {
+        console.log("controller")
         const user = await User.create(req.body)
-        res.send(user)
+        res.send(req.body)
     } catch (error) {
 
     }
--- backend/db/coon.js
+++ backend/db/coon.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-    const uri = "mongodb+srv://Pradeep:DXFpHCF0SPnlzGxb@yogshalacluster.4shslk2.mongodb.net/?retryWrites=true&w=majority";
-    mongoose.connect(uri,
-        {
-            useNewUrlParser: true,
-          useUnifiedTopology: true
-        }
-      );
-    
-    
-      const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error: "));
-    db.once("open", function () {
-      console.log("Connected successfully");
-    });
-
-}
+// const connectDatabase = () => {
+//   const uri = "mongodb+srv://Pradeep:DXFpHCF0SPnlzGxb@yogshalacluster.4shslk2.mongodb.net/?retryWrites=true&w=majority";
+//   const uri2= 'mongodb://127.0.0.1:27017/myapp'
+//   mongoose.connect(uri2,
+//     {
+//       useNewUrlParser: true,
+//     }
+//   );
 
 
-module.exports = { connectDatabase };
+//   const db = mongoose.connection;
+//   db.on("error", console.error.bind(console, "connection error: "));
+//   db.once("open", function () {
+//     console.log("Connected successfully");
+//   });
+
+// }
+
+
+// module.exports = { connectDatabase };
 
 
 
@@ -29,4 +29,20 @@ module.exports = { connectDatabase };
 
 
 
+// const mongoose = require("mongoose");
+
+  const connectDatabase = () => {
+  mongoose
+    .connect("mongodb://localhost:27017/yogshala", {
+      useNewUrlParser: true,
+    })
+    .then((data) => {
+      console.log(`mongodb connected with server ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { connectDatabase };
 
