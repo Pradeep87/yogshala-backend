dbde09a33be851c44fd4d78e9b6eca4288fcf379
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-03-30 09:49:30 +0530

    routes and controllers added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ const express = require("express")
 require('dotenv').config();
 const app = express()
 
+
+const userRoutes = require("./routes/user")
+
+app.use("/api/user", userRoutes)
+
+
+
 app.get("/", (req, res) => {
     res.send("hello server in running")
 })
