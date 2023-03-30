3d3c35747cb558214e08f701828aad48e7482d3f
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-03-30 15:17:17 +0530

    user schema methods added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
 
@@ -20,11 +22,11 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        default: "male"
+        required: [true, "Select Your Gender"],
     },
     birthDay: {
         type: Date,
-        required: [false, "Please Enter Your Date of Birth"],
+        required: [true, "Please Enter Your Date of Birth"],
     },
     role: {
         type: String,
@@ -62,6 +64,48 @@ const userSchema = new mongoose.Schema({
         default: Date.now,
     },
 })
+
+
+
+// protect password 
+userSchema.pre("save", async function name(next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+
+// jwt token 
+userSchema.methods.getJWTToken = function () {
+
+    return jwt.sign({ id: this._id }, process.env.SECRET_KEY, {
+        expiresIn: process.env.TOKEN_EXPIRE
+    })
+}
+
+// compare password 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+    // Generating Token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // Hashing and adding resetPasswordToken to userSchema
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+    return resetToken;
+};
+
 
 
 
