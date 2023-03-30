7237dcd1981fa625c2060d9e2059a51a32bd54a6
Pradeep Rajput <pradeep87055@gmail.com>
2023-03-30 21:30:08 +0530

    cluster adde

--- backend/db/coon.js
+++ backend/db/coon.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const password = process.env.PASSWORD
 
 
 const connectDatabase = () => {
-  const uri = `mongodb+srv://Pradeep:${password}@yogshalacluster.4shslk2.mongodb.net/?retryWrites=true&w=majority`;
-  mongoose.connect(uri,{useNewUrlParser: true,});
+  const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.PASSWORD}@yogshalacluster.4shslk2.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+  mongoose.connect(uri, { useNewUrlParser: true, });
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error: "));
   db.once("open", function () {
@@ -16,24 +15,3 @@ const connectDatabase = () => {
 
 module.exports = { connectDatabase };
 
-
-
-// useFindAndModify: false,
-
-// const mongoose = require("mongoose");
-
-//   const connectDatabase = () => {
-//   mongoose
-//     .connect("mongodb://localhost:27017/yogshala", {
-//       useNewUrlParser: true,
-//     })
-//     .then((data) => {
-//       console.log(`mongodb connected with server ${data.connection.host}`);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
-
-// module.exports = { connectDatabase };
-// .env example
