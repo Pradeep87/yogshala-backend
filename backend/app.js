fd5469fab4ee0bbd15c6a24b1d749c0b5c37d0e5
Pradeep Rajput <pradeep87055@gmail.com>
2023-04-17 20:32:30 +0530

    get user all data by id api added

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,33 @@
-
-const express = require("express")
-require('dotenv').config();
-const app = express()
-const errorMiddelware = require('./middelwares/error')
-const cookieParser = require('cookie-parser');
-const bodyparser = require('body-parser');
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const errorMiddelware = require("./middelwares/error");
+const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-const userRoutes = require("./routes/user")
-const postRoutes = require("./routes/post")
-const healthIssueRoutes = require("./routes/healthIssue")
-const treatmentPlan = require("./routes/treatmentPlan")
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const healthIssueRoutes = require("./routes/healthIssue");
+const treatmentPlan = require("./routes/treatmentPlan");
+const common = require("./routes/common");
 
 app.use(cookieParser());
-app.use(express.json())
+app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-
-
-app.use("/api/user", userRoutes)
-app.use("/api/post", postRoutes)
-app.use("/api/healthissue", healthIssueRoutes)
-app.use("/api/treatmentPlan", treatmentPlan)
-
-
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/healthissue", healthIssueRoutes);
+app.use("/api/treatmentPlan", treatmentPlan);
+app.use("/api/user", common);
 
 app.get("/", (req, res) => {
-    res.send("hello server in running")
-})
-
+  res.send("hello server in running");
+});
 
 // middeleware for error
 app.use(errorMiddelware);
 
-
-
-module.exports = app
+module.exports = app;
---
+++ backend/controllers/common.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+const HealthIssue = require("../models/healthIssues");
+const Post = require("../models/post");
+
+const catchAsyncError = require("../middelwares/catchAsyncError");
+const ErrorHandler = require("../utils/errorHandler");
+const sendToken = require("../utils/jwtToken");
+const cloudinary = require("cloudinary");
+
+exports.getUserData = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  const posts = await Post.find({ user: req.params.id });
+  const healthIssues = await HealthIssue.find({ user: req.params.id });
+  res.status(200).json({
+    success: true,
+    user,
+    posts,
+    healthIssues,
+  });
+});
