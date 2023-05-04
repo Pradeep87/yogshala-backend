9ddd6c2a8c42aad400c4e78de7a8fb54b8cbac4a
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-05-04 11:22:00 +0530

    get post by id modified

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,73 +3,71 @@ const Like = require("../models/likes");
 const Comment = require("../models/comments");
 const catchAsyncError = require("../middelwares/catchAsyncError");
 const cloudinary = require("cloudinary");
-const Notification = require('../models/notification')
+const Notification = require("../models/notification");
 
 exports.doComment = catchAsyncError(async (req, res, next) => {
   const pusher = req.app.get("pusher");
-  const { post, commentContent,parentCommentId } = req.body;
+  const { post, commentContent, parentCommentId } = req.body;
 
-if(!parentCommentId){
-  const comment = await Comment.create({
-    user: req.user._id,
-    post,
-    commentContent,
-  });
-  const isPost = await Post.findByIdAndUpdate(
-    comment.post,
-    { $push: { comments: comment._id }, $inc: { commentsCount: 1 } },
-    { new: true }
-  );
-  if (req.user._id.toString() !== isPost.user.toString()) {
-    const notification = {
-      user: isPost.user,
-      redirectPath: post,
-      notifType: "comment",
-      message: `${req.user.firstName} ${req.user.surname} commented on your Post`,
+  if (!parentCommentId) {
+    const comment = await Comment.create({
+      user: req.user._id,
+      post,
+      commentContent,
+    });
+    const isPost = await Post.findByIdAndUpdate(
+      comment.post,
+      { $push: { comments: comment._id }, $inc: { commentsCount: 1 } },
+      { new: true }
+    );
+    if (req.user._id.toString() !== isPost.user.toString()) {
+      const notification = {
+        user: isPost.user,
+        redirectPath: post,
+        notifType: "comment",
+        message: `${req.user.firstName} ${req.user.surname} commented on your Post`,
+      };
+      const isCreated = await Notification.create(notification);
+      pusher.trigger(`post-${isPost.user}`, "onPost", isCreated);
     }
-    const isCreated = await Notification.create(notification)
-    pusher.trigger(`post-${isPost.user}`, "onPost", isCreated);
-  }
-  res.json({
-    success: true,
-    message: "Comment Added",
-  });
-}else{
+    res.json({
+      success: true,
+      message: "Comment Added",
+    });
+  } else {
+    const parentComment = await Comment.findById(parentCommentId);
 
-  const parentComment = await Comment.findById(parentCommentId);
+    if (!parentComment) {
+      return res.status(404).json({
+        success: false,
+        message: "Parent comment not found",
+      });
+    }
 
-  if (!parentComment) {
-    return res.status(404).json({
-      success: false,
-      message: "Parent comment not found",
+    const reply = await Comment.create({
+      user: req.user._id,
+      post,
+      commentContent,
+      parentComment: parentCommentId,
+    });
+
+    parentComment.replies.push(reply._id);
+    await parentComment.save();
+    if (req.user._id.toString() !== parentComment.user.toString()) {
+      const notification = {
+        user: parentComment.user,
+        redirectPath: post,
+        notifType: "comment",
+        message: `${req.user.firstName} ${req.user.surname} replies on your comment`,
+      };
+      const isCreated = await Notification.create(notification);
+      pusher.trigger(`post-${parentComment.user}`, "onPost", isCreated);
+    }
+    res.json({
+      success: true,
+      message: "Reply added",
     });
   }
-
-  const reply = await Comment.create({
-    user: req.user._id,
-    post,
-    commentContent,
-    parentComment: parentCommentId,
-  });
-
-  parentComment.replies.push(reply._id);
-  await parentComment.save();
-  if (req.user._id.toString() !== parentComment.user.toString()) {
-    const notification = {
-      user: parentComment.user,
-      redirectPath: post,
-      notifType: "comment",
-      message: `${req.user.firstName} ${req.user.surname} replies on your comment`,
-    }
-    const isCreated = await Notification.create(notification)
-    pusher.trigger(`post-${parentComment.user}`, "onPost", isCreated);
-  }
-  res.json({
-    success: true,
-    message: "Reply added",
-  });
-}
- 
 });
 
 exports.doLike = catchAsyncError(async (req, res, next) => {
@@ -85,12 +83,12 @@ exports.doLike = catchAsyncError(async (req, res, next) => {
     );
     return res.json({
       success: true,
-      message: 'Like removed successfully',
+      message: "Like removed successfully",
     });
   }
 
   const like = await Like.create({ user: req.user._id, post, reaction });
-const isPost= await Post.findByIdAndUpdate(
+  const isPost = await Post.findByIdAndUpdate(
     like.post,
     { $push: { likes: like._id }, $inc: { likesCount: 1 } },
     { new: true }
@@ -102,14 +100,14 @@ const isPost= await Post.findByIdAndUpdate(
       redirectPath: post,
       notifType: "comment",
       message: `${req.user.firstName} ${req.user.surname} liked your Post`,
-    }
-    const isCreated = await Notification.create(notification)
+    };
+    const isCreated = await Notification.create(notification);
     pusher.trigger(`post-${isPost.user}`, "onPost", isCreated);
   }
   res.json({
     success: true,
     like,
-  }); 
+  });
 });
 
 exports.doPost = catchAsyncError(async (req, res, next) => {
@@ -133,21 +131,31 @@ exports.doPost = catchAsyncError(async (req, res, next) => {
   });
 });
 
-
-
 exports.getPostById = catchAsyncError(async (req, res, next) => {
-  const post = await Post.findById({ _id:req.params.id })
-    .populate("likes")
-    .populate("comments");
+  const post = await Post.findById({ _id: req.params.id })
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: ["avatar", "firstName", "surname", "_id"],
+      },
+    })
+    .populate({
+      path: "likes",
+      populate: {
+        path: "user",
+        select: ["avatar", "firstName", "surname", "_id"],
+      },
+    })
+    .populate({
+      path: "user",
+      select: ["avatar", "firstName", "surname", "_id"],
+    });
   res.json({
     success: true,
     post,
   });
 });
-
-
-
-
 
 exports.getUserPost = catchAsyncError(async (req, res, next) => {
   const posts = await Post.find({ user: req.user._id })
@@ -171,11 +179,12 @@ exports.getTimelinePost = catchAsyncError(async (req, res, next) => {
       },
     })
     .populate({
-      path:"likes",
-      populate:{
+      path: "likes",
+      populate: {
         path: "user",
         select: ["avatar", "firstName", "surname", "_id"],
-      }})
+      },
+    })
     .populate({
       path: "user",
       select: ["avatar", "_id", "firstName", "surname"],
